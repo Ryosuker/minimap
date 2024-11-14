d10e1b7da639912251e5e1bf28474e635f925cc4
Ryosuker <ryowada290@gmail.com>
2024-11-14 23:58:00 +0900

    マイグレーションファイル

--- db/migrate/20241114144631_create_work_places.rb
+++ db/migrate/20241114144631_create_work_places.rb
@@ -1,6 +1,15 @@
 class CreateWorkPlaces < ActiveRecord::Migration[7.0]
   def change
     create_table :work_places do |t|
+      t.string :type, null: false
+      t.string :name, null: false
+      t.string :postal_code
+      t.string :address
+      t.string :phone_number
+      t.string :opening_hours
+      t.decimal :latitude, precision: 10, scale: 7, null: false
+      t.decimal :longitude, precision: 10, scale: 7, null: false
+      t.string :place_id, null: false
 
       t.timestamps
     end
