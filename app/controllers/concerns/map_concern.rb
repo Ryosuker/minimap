13ef4e3d4b0b51877e3a692fe33a1663b8c108db
Ryosuker <ryowada290@gmail.com>
2024-11-16 17:28:38 +0900

    コントローラ修正

---
+++ app/controllers/concerns/map_concern.rb
@@ -0,0 +1,13 @@
+module MapConcern
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_map_data, only: :home
+  end
+
+  def set_map_data
+    gon.latitude = 35.6813843233819
+    gon.longitude = 139.76712479697295
+    gon.api_key = ENV['GOOGLE_MAPS_API_KEY']
+  end
+end
